3691d4836e257246bddcf3acfe1121a068bb431f
VickyVignesh2002 <itzmevicky111@gmail.com>
2026-02-20 22:47:30 +0530

    Fix Python 3.13 dependency resolution and NumPy 2 imgaug compatibility

--- edocr2/keras_ocr/tools.py
+++ edocr2/keras_ocr/tools.py
@@ -7,8 +7,18 @@ import urllib.request
 import urllib.parse
 
 import cv2
-import imgaug
 import numpy as np
+
+if not hasattr(np, "sctypes"):
+    np.sctypes = {
+        "int": [np.int8, np.int16, np.int32, np.int64],
+        "uint": [np.uint8, np.uint16, np.uint32, np.uint64],
+        "float": [np.float16, np.float32, np.float64],
+        "complex": [np.complex64, np.complex128],
+        "others": [np.bool_, np.object_, np.str_, np.bytes_],
+    }
+
+import imgaug
 import validators
 import typing_extensions as tx
 import matplotlib.pyplot as plt
